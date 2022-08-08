6d4a197c7bb6dcf87b2bb98ccc71f283086acb0a
ArthKumar <48092253+KoderKumar@users.noreply.github.com>
2022-08-08 15:36:27 +0530

    Add files via upload

---
+++ cube-list-generator.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number where you want to start: "))
+t = int(input("Enter a number where you want to end: "))
+
+lst = []
+for x in range(x,y + 1):
+	lst.append(x ** 3)
+print(lst)
